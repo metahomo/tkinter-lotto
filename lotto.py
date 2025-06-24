320b7b1602be5ae8109156887f1592b30e307d09
metahomo <nlpwhiz@gmail.com>
2025-06-24 10:49:23 +0900

    lotto

---
+++ lotto.py
@@ -0,0 +1,12 @@
+import random
+
+lotto_num = range(1, 46)
+
+for i in range(5) :
+    print(ramdom.smple(lotto_num, 6))
+    
+import tkinter
+import tkinter.font
+import random
+
+lotto_num = range(1, 46)
